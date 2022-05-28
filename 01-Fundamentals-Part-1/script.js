4cffe6c84a381e7c44909beab8834fab877589a1
renukumar07 <renukumar90@gmail.com>
2022-05-28 18:03:15 +0530

    15. Operator Precedence

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -63,6 +63,7 @@ lastName = 'Schmedtmann';
 console.log(lastName);
 */
 
+/*
 // Math operators
 const now = 2037;
 const ageJonas = now - 1991;
@@ -72,8 +73,8 @@ console.log(ageJonas, ageSara);
 console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
 
-const firstName = ' Jonas ';
-const lastName = ' Schmedtmann ';
+const firstName = 'Jona';
+const lastName = 'Schmedtmann';
 console.log(firstName + ' ' + lastName);
 
 // Assignment operators
@@ -90,3 +91,16 @@ console.log(ageJonas > ageSarah); // >,<,>=,<=
 console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSara = now - 2018;
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas - agesarah) / 2;
+console.log(ageJonas, agesarah, averageAge);
