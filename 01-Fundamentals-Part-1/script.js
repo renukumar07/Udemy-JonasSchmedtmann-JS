75c3033c2b2f1e0a27f0e9cb693d59621346bad6
renukumar07 <renukumar90@gmail.com>
2022-05-29 13:55:49 +0530

    18. Taking Decisions: if / else Statements

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -147,6 +147,7 @@ console.log(markHigherBMI);
 
 */
 
+/*
 const firstName = 'Jonas';
 const job = 'teacher';
 const birthYear = 1991;
@@ -165,3 +166,23 @@ console.log('String with \n multiple \n lines');
 console.log(`String with 
 multiple 
 lines`);
+*/
+
+const age = 15;
+
+if (age >= 18) {
+  console.log('Sarah can start her driving license 🚗');
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 2012;
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+
+console.log(century);
