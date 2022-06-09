d10a9241bbbae199d76588c0b69ad1e449a0fc3b
renukumar07 <renukumar90@gmail.com>
2022-06-09 18:06:15 +0530

    33. Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -8,3 +8,28 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio';
 // const private = 534;
+*/
+
+function logger() {
+  console.log('My name is Jonas');
+}
+
+//calling / running / invoking function
+logger();
+logger();
+logger();
+
+function foodProcessor(apple, orange) {
+  const juice = `Juice with ${apple} apples and ${orange} orangess`;
+  return juice;
+}
+
+const appleJuice = foodProcessor(5, 0);
+console.log(appleJuice);
+console.log(foodProcessor(5, 0));
+
+const appleOrangeJuice = foodProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23');
+console.log(num);
