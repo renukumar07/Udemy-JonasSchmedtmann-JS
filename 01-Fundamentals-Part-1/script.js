879cf3e2a970bed51c1eb883349dc8c4df0de6ae
renukumar07 <renukumar90@gmail.com>
2022-06-09 13:35:31 +0530

    29. Coding Challenge #4

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -433,7 +433,7 @@ if (23 > 10) {
 const me = 'Jonas';
 console.log(`I'm ${2037 - 1991} years old ${me}`);
 */
-
+/*
 const age = 23;
 age >= 18
   ? console.log('I like to drink wine')
@@ -451,3 +451,19 @@ if (age >= 18) {
 console.log(drink);
 
 console.log(`i like to drink ${age >= 18 ? 'wine' : 'water'}`);
+*/
+
+/*
+Steven wants to build a very simple tip calculator for whenever he goes eating in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+Your tasks:
+1. Calculatethetip,dependingonthebillvalue.Createavariablecalled'tip'for this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
+2. Printastringtotheconsolecontainingthebillvalue,thetip,andthefinalvalue (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value 316.25”
+Test data:
+§ Data 1: Test for bill values 275, 40 and 430 Hints:
+§ To calculate 20% of a value, simply multiply it by 20/100 = 0.2 § Value X is between 50 and 300, if it's>= 50 && <= 300😉
+*/
+
+tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+);
