e3eb891b81b1ba4946a785947639fa82993afa2d
renukumar07 <renukumar90@gmail.com>
2022-06-09 18:19:59 +0530

    35. Arrow Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -36,6 +36,7 @@ const num = Number('23');
 console.log(num);
 */
 
+/*
 //Function declaration
 function calcAge1(birthYear) {
   return 2037 - birthYear;
@@ -49,3 +50,19 @@ const calcAge2 = function (birthYear) {
 };
 const age2 = calcAge2(1991);
 console.log(age1, age2);
+*/
+
+//Arrow function
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
