d73716f8377ffa58093a63643ea5076a8f6c90ee
renukumar07 <renukumar90@gmail.com>
2022-06-16 15:26:08 +0530

    41. Coding challenge #2

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -188,6 +188,7 @@ const ages = [
 console.log(ages);
 */
 
+/*
 const friends = ['Michael', 'Steven', 'Peter'];
 
 // Add elements
@@ -216,3 +217,35 @@ console.log(friends.includes('23'));
 if (friends.includes('Steven')) {
   console.log('You have a friend called Steven');
 }
+*/
+
+// Coding Challenge #2
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+// Your tasks:
+// 1. Writeafunction'calcTip'thattakesanybillvalueasaninputandreturns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100
+// 2. Andnowlet'susearrays!Socreateanarray'bills'containingthetestdata below
+// 3. Createanarray'tips'containingthetipvalueforeachbill,calculatedfrom the function you created before
+// 4. Bonus:Createanarray'total'containingthetotalvalues,sothebill+tip Test data: 125, 555 and 44
+// Hint: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+// GOOD LUCK 😀
+
+const calcTip = function (billValue) {
+  if (billValue >= 50 && billValue <= 300) {
+    return billValue * 0.15;
+  } else {
+    return billValue * 0.2;
+  }
+};
+
+let bills = [125, 555, 44];
+let tips = [];
+tips.push(calcTip(bills[0]));
+tips.push(calcTip(bills[1]));
+tips.push(calcTip(bills[2]));
+console.log(tips);
+
+let total = [];
+total.push(bills[0] + calcTip(bills[0]));
+total.push(bills[1] + calcTip(bills[1]));
+total.push(bills[2] + calcTip(bills[2]));
+console.log(total);
