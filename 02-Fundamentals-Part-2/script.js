4b63fe923e1b404bc87595804e246fb21ebd0cb6
renukumar07 <renukumar90@gmail.com>
2022-06-18 17:29:54 +0530

    48. Looping Backwards and Loops in Loops

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -398,6 +398,7 @@ for (let rep = 1; rep <= 30; rep++) {
 }
 */
 
+/*
 const jonas = [
   'Jonas',
   'Schmedtmann',
@@ -437,4 +438,30 @@ console.log('--- BREAK WITH NUMBER ---');
 for (let i = 0; i < jonas.length; i++) {
   if (typeof jonas[i] === 'number') break;
   console.log(jonas[i], typeof jonas[i]);
+}
+
+*/
+
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true,
+];
+
+//0,1, ..., 4
+//4,3, ..., 0
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`---------- Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
+  }
 }
